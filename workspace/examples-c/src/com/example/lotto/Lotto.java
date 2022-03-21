9dcc74222c1b0271bf46d8e85146f5ede81ed444
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-03-22 01:01:49 +0900

    Complete LottoGame

--- workspace/examples-c/src/com/example/lotto/Lotto.java
+++ workspace/examples-c/src/com/example/lotto/Lotto.java
@@ -1,22 +1,22 @@
 package com.example.lotto;
+
 /**
- * 
  * 로또 당첨 예상 번호 뽑기 클래스
  * @author Administrator
  *
  */
 public class Lotto {
-	// satic을 다 할거면 CLASS 왜 함
-	// static method 안에서는 static 변수만
-	// numbers = new int[6]
+	
 	private int[] selectBasicNumbers() {
-		int [] numbers = new int[6]; // 정수 6개를 저장할 수 있는 배열 인스턴 만들고 주소를 저장
-		for(int i =0; i < numbers.length; i++) {
-			numbers[i] = (int)(Math.random() * 45)+1;
-			for(int j=0; j < i; j++) {
-				if(numbers[i] == numbers[j]) { // 중복된 데이터가 발견된다면
-					// i = -1 을 넣음으로써 초기화를 시킬 수 있음;
-					i--; // i-=1, --i, i= i-1;
+		// int[] numbers; // 배열 참조변수 만들기
+		// numbers = new int[6]; // 정수 6개를 저장할 수 있는 배열 인스턴스 만들고 주소를 저장
+		int[] numbers = new int[6];
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = (int)(Math.random() * 45) + 1;
+			for (int j = 0; j < i; j++) { // 중복 검사를 위한 반복문
+				if (numbers[i] == numbers[j]) { // 중복된 데이터 발견된다면
+					//i = -1;
+					i--; // i -= 1, --i, i = i - 1
 					break;
 				}
 			}
@@ -24,30 +24,36 @@ public class Lotto {
 		return numbers;
 	}
 	
-	private boolean checkAverage (int[] numbers, int from, int to) {
+	private boolean checkAverage(int[] numbers, int from, int to) {
 		int sum = 0;
-		// 인헨스드 for 문 ?
-		for(int number : numbers) {
-			sum += number;
+		for (int number : numbers) {
+			sum += number; // sum = sum + number;
 		}
-		
-		// 앞은 포함 뒤는 안 포함 관행이다;
 		int mean = sum / numbers.length;
+		
 		return mean >= from && mean < to;
 	}
 	
-	public int[] selectWinningNumber () {
+	
+	public int[] selectWinningNumbers() {
 		int[] numbers = null;
 		boolean valid = false;
-		//실행 결과의 경우 do while 문
 		
 		do {
-			 numbers = selectBasicNumbers();
-			 valid = checkAverage(numbers,20, 26);
-		} while(valid == false);
+			numbers = selectBasicNumbers();
+			valid = checkAverage(numbers, 20, 26);
+		} while (valid == false);
 		
 		return numbers;
-		
 	}
+	
 
 }
+
+
+
+
+
+
+
+
--- workspace/examples-c/src/com/example/lotto/LottoGame.java
+++ workspace/examples-c/src/com/example/lotto/LottoGame.java
@@ -5,44 +5,56 @@ import java.util.Scanner;
 public class LottoGame {
 	
 	private Scanner scanner = new Scanner(System.in);
+	
 	/**
-	 * 
-	 * @return
+	 * 사용자에게 선택할 수 있는 작업목록을 표시하고<br>
+	 * 사용자의 선택을 받아서 반환하는 메서드
+	 * @return 사용자가 선택한 작업 번호
 	 */
 	private String selectMenu() {
-//		java.util.Scanner scanner = new java.util.Scanner(System.in); // 입력기 생성
-		System.out.println("****************");
-		System.out.println("1. 당첨 예상 번호 뽑기.");
-		System.out.println("2. 종류");
-		System.out.println("****************");
-		System.out.println("작업을 선택하세요 : ");
-		String selection = scanner.nextLine(); // 사용자 입력을 문자ㅕㄹ(0개 이상의 문자 집합)로 변환
-		System.out.println("Your selection is "+ selection);
+		System.out.println("******************************");
+		System.out.println("* 1. 당첨 예상 번호 뽑기.");
+		System.out.println("* 2. 종료.");
+		System.out.println("******************************");
+
+		System.out.print("작업을 선택하세요 : ");
+		String selection = scanner.nextLine(); // 사용자 입력을 문자열(0개 이상의 문자 집합)로 반환
+		// System.out.println("your selection is " + selection); // 문자열 + 문자열 -> 문자열 결합
 		return selection;
 	}
 	
-	
-	/**
-	 * 
-	 * @param numbers
-	 */
 	private void showNumbers(int[] numbers) {
-		System.out.println("당첨 예상 번호 :");
-		for(int i = 0; i < numbers.length; i++) {
-			System.out.println(i);
-			System.out.println("[" +numbers[i] +"]");
+		System.out.print("당첨 예상 번호 : ");
+		for(int i = 0; i < numbers.length; i++) { // i++ : i = i + 1
+			System.out.print("[" + numbers[i] + "]");
 		}
+		System.out.println();
 	}
 	
 	public void doGame() {
-		Lottol lotto = new Lotto ();
+		Lotto lotto = new Lotto();
+		while (true) {
+			
+			String selection = selectMenu();
+			
+			if (selection.equals("1")) {
+				
+				int[] numbers = lotto.selectWinningNumbers();				
+				showNumbers(numbers);
+				
+			} else if (selection.equals("2")) {
+				System.out.println("행운을 빕니다 !!!!!");
+				System.out.println("프로그램을 종료합니다.");
+				break; // while 반복문 종료
+			} else { // 비정상 입력
+				System.out.println("지원하지 않는 명령입니다. ");
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		LottoGame game = new LottoGame();
 		game.doGame();
-
 	}
 
 }
