b7365871667a5aa8d0ee6aa6376bc91e3f2a38a9
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-04-12 01:27:52 +0900

    Add examples-web project

---
+++ workspace/examples-web/src/main/java/com/exampleweb/filter/ExampleFilter.java
@@ -0,0 +1,27 @@
+package com.exampleweb.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter(urlPatterns = { "*.jsp" })
+public class ExampleFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest)request;
+		System.out.println(req.getRequestURI()); // 현재 요청 경로 출력
+		
+		chain.doFilter(request, response); // 다음 필터 또는 서블릿으로 요청 전달
+		
+	}
+
+}
