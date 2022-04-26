3594dbbed0dcedb3c21a81da3b095dba6201f50f
billy7175 <whgus439@gmail.com>
2022-04-26 20:32:32 +0900

    Java class

---
+++ workspace-spring/demo/src/main/java/controller/HomeController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+
+//@RestC
+//@Controller
+@Controller
+public class HomeController {
+	
+//	@GetMapping
+
+//	@GetMapping(path = {"/", "/home"})
+//	public Stirng home() {
+//		return 'Hello, Spring Boot Web Application';
+//	}
+	
+	@GetMapping
+}
